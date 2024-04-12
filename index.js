11e1ce9e2f6bd7425e89abe88d5d86b91eba2d70
Kaung Satt <kaungsatt.2k1@gmail.com>
2024-04-12 13:24:39 +0630

    js popup

---
+++ index.js
@@ -0,0 +1,19 @@
+const openBtn = document.querySelector(".btn-open");
+const modalBox = document.querySelector(".overlay");
+const closeBtn = document.querySelectorAll(".btn-close");
+const overlay = document.querySelector(".overlay");
+openBtn.addEventListener("click", () => {
+  modalBox.classList.add("show");
+});
+
+closeBtn.forEach((cbtn) => {
+  cbtn.addEventListener("click", () => {
+    modalBox.classList.remove("show");
+  });
+});
+overlay.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target === overlay) {
+    modalBox.classList.remove("show");
+  }
+});
